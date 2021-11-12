69d0bf21c3cfefd991bca0ab35d646189fce7a47
pvinaymadhur <vinaymadhur14@gmail.com>
2021-11-12 10:47:10 -0600

    Added resource changes

--- controllers/rollsroyce.js
+++ controllers/rollsroyce.js
@@ -12,9 +12,16 @@ exports.rollsroyce_list = async function(req, res) {
         } 
 };
 // for a specific Rollsroyce.
-exports.rollsroyce_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Rollsroyce detail: ' + req.params.id);
-};
+exports.rollsroyce_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Rollsroyce.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
 // Handle Rollsroyce create on POST.
 // Handle Rollsroyce create on POST.
 exports.rollsroyce_create_post = async function(req, res) {
@@ -41,9 +48,25 @@ exports.rollsroyce_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Rollsroyce delete DELETE ' + req.params.id);
 };
 // Handle Rollsroyce update form on PUT.
-exports.rollsroyce_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Rollsroyce update PUT' + req.params.id);
-};
+exports.rollsroyce_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Rollsroyce.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.rollsroyce_type)
+    toUpdate.rollsroyce_type = req.body.rollsroyce_type;
+    if(req.body.cost) toUpdate.cost = req.body.cost;
+    if(req.body.model) toUpdate.model = req.body.model;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
 // VIEWS
 // Handle a show all view
 exports.rollsroyce_view_all_Page = async function(req, res) {
