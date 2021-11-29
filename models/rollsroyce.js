93edb1e79709f0c48f11079591306ebea9a2c551
pvinaymadhur <vinaymadhur14@gmail.com>
2021-11-29 13:35:30 -0600

    Added buttons

--- models/rollsroyce.js
+++ models/rollsroyce.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const rollsroyceSchema = mongoose.Schema({
 rollsroyce_type: String,
 model: String,
-cost: Number
+cost: { type: Number, min: 2000, max: 300000, default: 0 },
 })
 module.exports = mongoose.model("rollsroyce",
 rollsroyceSchema)
